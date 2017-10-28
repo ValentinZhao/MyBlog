56606cc485ad01f1792da339df916dbe7f77ed6d
ValentinZhao <2264709714@qq.com>
2017-10-28 10:55:09 +0800

    new api development finish

--- backend/controllers/api.js
+++ backend/controllers/api.js
@@ -1,14 +1,15 @@
 const Article = require('../model/article');
 
-const article = new Article();
+const ArticleDetail = require('../model/articledetail')
 
+const article = new Article();
+const article_detail = new ArticleDetail();
 
 
 
 module.exports = {
   'GET /api/articlelist': async (ctx, next) => {
     await article.queryAll().then(res => {
-      console.log(res)
       articleList = res;
       //设置Content-Type
       ctx.response.type = 'application/json';
@@ -16,6 +17,19 @@ module.exports = {
       //koa会自动把赋给ctx.response.body的对象转化为JSON并输出到客户端
       ctx.response.body = {
         articleList
+      }
+    })
+  },
+  'GET /api/articledetail': async (ctx, next) => {
+    const id_param = ctx.url.match(/id=(\w+)/g)[0];
+    const id = id_param.substring(id_param.indexOf('=') + 1);
+    await article_detail.queryById(id).then(res => {
+      const res_detail = res;
+      ctx.response.type = 'application/json';
+      ctx.response.set('Access-Control-Allow-Origin', '*');
+      //koa会自动把赋给ctx.response.body的对象转化为JSON并输出到客户端
+      ctx.response.body = {
+        res_detail
       }
     })
   }
